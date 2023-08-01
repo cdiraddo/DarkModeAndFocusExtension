55f3057632ac4cfea6b5eb2fdbdf5a45d29ed924
Charles DiRaddo <68091434+cdiraddo@users.noreply.github.com>
2023-07-31 22:12:45 -0500

    DarkMode button and Popup added

---
+++ popup.js
@@ -0,0 +1,16 @@
+// Function to handle toggle button change
+function handleToggle() {
+    const toggleButton = document.getElementById('toggleButton');
+    const statusDiv = document.getElementById('status');
+    
+    if (toggleButton.checked) {
+      // Code to run when the button is toggled on
+      statusDiv.textContent = 'Button is ON';
+    } else {
+      // Code to run when the button is toggled off
+      statusDiv.textContent = 'Button is OFF';
+    }
+  }
+  
+  // Event listener for the toggle button
+  document.getElementById('toggleButton').addEventListener('change', handleToggle);
