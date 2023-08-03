2386b64cc2f78d1ad9b594017247ca8ddbf24dad
Charles DiRaddo <68091434+cdiraddo@users.noreply.github.com>
2023-08-03 15:07:57 -0500

    Added more darkmode support

--- scripts/content.js
+++ scripts/content.js
@@ -5,6 +5,8 @@ const site = window.location.hostname
 //Custom CSS Func
 const Add_Custom_Style = css => document.head.appendChild(document.createElement("style")).innerHTML = css
 
+//SEPERATE OUT TO DEFINE CSS ELEMENTS FOR LATER EDITING
+//Fix Rulers: covering up text
 if (site.includes("docs.google.com")) {
     Add_Custom_Style(`
   
@@ -15,10 +17,11 @@ if (site.includes("docs.google.com")) {
         .docs-grille-gm3.docs-gm .docs-material #docs-toolbar-wrapper {
         background-color: #202124;
         }
+
         .docs-grille-gm3 #docs-chrome:not(.docs-hub-chrome) {
         background: #202124;
         }
-  
+
         .kix-appview-editor {
         background-color: #202124 !important;
         }
@@ -26,6 +29,28 @@ if (site.includes("docs.google.com")) {
         .kix-canvas-tile-content {
         background: transparent !important;
         }
+
+        .docs-horizontal-ruler {
+          background-color: #202124 !important;
+        }
+
+        .companion-guest-app-switcher {
+          background-color: #202124 !important;
+        }
+
+        .companion-collapser-button {
+          background-color: #202124 !important;
+        }
+
+        .docs-grille-gm3 .docs-material #docs-header .docs-titlebar-buttons {
+          background-color: #202124 !important;
+        }
+
+        .docs-ruler-background, .docs-ruler-face-inner, .docs-ruler-mask {
+          background-color: #202124 !important;
+        }
+
+
   
   
   
@@ -33,5 +58,6 @@ if (site.includes("docs.google.com")) {
   
     `)
 }
+
 
 
