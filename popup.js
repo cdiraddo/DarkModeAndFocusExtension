25f7deae4eb02e262226b9e56b08a608fe13e2ce
Charles DiRaddo <68091434+cdiraddo@users.noreply.github.com>
2023-08-03 14:51:00 -0500

    button updated (still doesnt work

--- popup.js
+++ popup.js
@@ -1,17 +1,6 @@
-// Function to handle toggle button change
-var buttontog = false;
-function handleToggle() {
-    const toggleButton = document.getElementById('toggleButton');
-    const statusDiv = document.getElementById('status');
-    
-    if (toggleButton.checked) {
-      // Code to run when the button is toggled on
-      statusDiv.textContent = 'Button is ON';
-    } else {
-      // Code to run when the button is toggled off
-      statusDiv.textContent = 'Button is OFF';
+const button = document.querySelector("button");
+
+button.addEventListener("click", async () => {
+    // alert("buttonhasbeenclicked")
     }
-  }
-  
-  // Event listener for the toggle button
-  document.getElementById('toggleButton').addEventListener('change', handleToggle);
+);
--- scripts/content.js
+++ scripts/content.js
@@ -1,51 +1,37 @@
 //Get URl
 const site = window.location.hostname
 
-// alert("Javascript injected into " + site)
 
 //Custom CSS Func
 const Add_Custom_Style = css => document.head.appendChild(document.createElement("style")).innerHTML = css
 
-//JS For Docs
 if (site.includes("docs.google.com")) {
-  alert("injecting css")
-  // document.getElementById().style.property =
-  Add_Custom_Style(`
-
-    * {
-      background_color: #0F79C6 !important;
-    }
-
-    .docs-grille-gm3.docs-gm .docs-material #docs-toolbar-wrapper {
-      background-color: #202124;
-    }
-    .docs-grille-gm3 #docs-chrome:not(.docs-hub-chrome) {
-      background: #202124;
-    }
-
-    .kix-appview-editor {
-      background-color: #202124 !important;
-    }
-
-    .kix-canvas-tile-content {
-      background: transparent !important;
-    }
-
-
-
-
-
-  `)
-
-
-
+    Add_Custom_Style(`
+  
+        * {
+        background_color: #0F79C6 !important;
+        }
+  
+        .docs-grille-gm3.docs-gm .docs-material #docs-toolbar-wrapper {
+        background-color: #202124;
+        }
+        .docs-grille-gm3 #docs-chrome:not(.docs-hub-chrome) {
+        background: #202124;
+        }
+  
+        .kix-appview-editor {
+        background-color: #202124 !important;
+        }
+  
+        .kix-canvas-tile-content {
+        background: transparent !important;
+        }
+  
+  
+  
+  
+  
+    `)
 }
 
-    // body,
-    // .docs-gm #docs-editor,
-    // .docs-gm #docs-editor-container,
-    // .docs-gm:not(.docs-grille-gm3) .kix-appview-editor,
-    // .apps-shortcutshelpcontentimpl-container,
-    // .apps-shortcutshelppopup {
-    //   background: #202124 !important;
-    // }
+
